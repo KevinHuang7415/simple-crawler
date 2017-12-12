3241022a73837e38744d7752aea311ec4077b0a8
Kevin Huang <goliath7415@gmail.com>
2017-12-12 21:22:16 +0800

    Check if the date of earliest article is expired,
    which is defined as whether the date is at least 15 days before

--- SoftJob_Crawler.py
+++ SoftJob_Crawler.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 import file_helper
+import datetime_helper
 
 PTT_URL = 'https://www.ptt.cc'
 SOFTJOB_URI = '/bbs/Soft_Job/index.html'
@@ -21,8 +22,10 @@ def crawler():
     curr_page_url = SOFTJOB_URI
 
     board_page = get_web_page(curr_page_url)
+
     if board_page:
-        articles_meta = get_articles_meta(board_page)
+        curr_page_url, articles_meta = get_articles_meta(board_page)
+        global LATEST_PAGE
         LATEST_PAGE = False
 
         for article_meta in articles_meta:
@@ -43,21 +46,9 @@ def get_web_page(url):
 def get_articles_meta(dom):
     soup = BeautifulSoup(dom, 'html.parser')
 
-
-    def find_prev_page_url(soup):
-        div_paging = soup.find('div', 'btn-group btn-group-paging')
-        # 0: earliest, 1: previous, 2: next, 3: latest
-        btn_prev_page = div_paging.find_all('a')[1]
-    
-        if 'href' in btn_prev_page.attrs:
-            return btn_prev_page['href']
-        else:
-            return None
-
-    prev_page_url = find_prev_page_url(soup)
-
     # articles under separation (aka pinned posts) should be ignored
     list_sep = soup.find('div', 'r-list-sep')
+    global LATEST_PAGE
     if LATEST_PAGE and list_sep:
         divs = list_sep.find_all_previous('div', 'r-ent')
         # reserve to the original order
@@ -72,7 +63,6 @@ def get_articles_meta(dom):
         if prop_a:
             href = prop_a['href']
             title = prop_a.text
-
             # date format mm/dd and prefix for m is space instead of 0
             date = dom.find('div', 'date').text.strip()
             return title, href, date
@@ -90,7 +80,17 @@ def get_articles_meta(dom):
                 'date': date
             })
 
-    return articles_meta
+    if datetime_helper.check_expired(articles_meta[0]['date']):
+        return None, articles_meta
+    else:
+        div_paging = soup.find('div', 'btn-group btn-group-paging')
+        # 0: earliest, 1: previous, 2: next, 3: latest
+        btn_prev_page = div_paging.find_all('a')[1]
+    
+        if 'href' in btn_prev_page.attrs:
+            return btn_prev_page['href'], articles_meta
+        else:
+            return None, articles_meta
 
 def get_article_content(url):
     article_page = get_web_page(url)
