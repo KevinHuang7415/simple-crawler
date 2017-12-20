ae1c2425269005d44b64b9c56c13865d75e6e709
Kevin Huang <goliath7415@gmail.com>
2017-12-20 19:24:45 +0800

    Pylint warning correction

--- SoftJob_Crawler.py
+++ SoftJob_Crawler.py
@@ -1,3 +1,7 @@
+'''
+Helper functions for file operations.
+'''
+
 import sys
 import time
 
@@ -10,15 +14,15 @@ import datetime_helper
 PTT_URL = 'https://www.ptt.cc'
 SOFTJOB_URI = '/bbs/Soft_Job/index.html'
 LATEST_PAGE = True
-DIR = ''
 
 
-def set_data_path():
-    global DIR
-    DIR = file_helper.get_dir(sys.argv)
-    file_helper.create_dir_if_not_exist(DIR)
+def setup_path():
+    '''Set path for saving files.'''
+    dir_path = file_helper.get_dir(sys.argv)
+    file_helper.create_dir_if_not_exist(dir_path)
 
 def crawler():
+    '''Grab all articles in recent days.'''
     curr_page_url = SOFTJOB_URI
 
     while curr_page_url:
@@ -33,9 +37,10 @@ def crawler():
                 article = get_article_content(article_meta['href'])
                 save_article(article, article_meta)
 
-def get_web_page(url, t = 0.5):
+def get_web_page(url, t=0.4):
+    '''Get web page content.'''
     # to avoid being detected as DDOS
-    time.sleep(t)  
+    time.sleep(t)
 
     resp = requests.get(PTT_URL + url)
     if resp.status_code != 200:
@@ -45,6 +50,7 @@ def get_web_page(url, t = 0.5):
         return resp.text
 
 def get_articles_meta(dom):
+    '''Retrieve meta for all articles in current page.'''
     soup = BeautifulSoup(dom, 'html.parser')
 
     # articles under separation (aka pinned posts) should be ignored
@@ -75,22 +81,24 @@ def get_articles_meta(dom):
         div_paging = soup.find('div', 'btn-group btn-group-paging')
         # 0: earliest, 1: previous, 2: next, 3: latest
         btn_prev_page = div_paging.find_all('a')[1]
-    
+
         if 'href' in btn_prev_page.attrs:
             return btn_prev_page['href'], articles_meta
         else:
             return None, articles_meta
 
 def remove_expired(articles_meta):
-    while len(articles_meta):
+    '''Remove data in dates which are expired.'''
+    while articles_meta:
         if datetime_helper.check_expired(articles_meta[0]['date']):
             articles_meta.pop(0)
         else:
             break
-    
+
     return articles_meta
 
 def get_article_meta(dom):
+    '''Get article meta.'''
     prop_a = dom.find('a')
 
     if prop_a:
@@ -103,6 +111,7 @@ def get_article_meta(dom):
         return None, None, None
 
 def get_article_content(url):
+    '''Get complete article content.'''
     article_page = get_web_page(url)
     if article_page:
         soup = BeautifulSoup(article_page, 'html.parser')
@@ -112,16 +121,18 @@ def get_article_content(url):
         return None
 
 def save_article(article, meta):
+    '''Save cached article to file.'''
     if article:
         # to avoid the titles collision
         # format PTT_URL/..../M.number.A.RND.html, take the number part
-        hash = meta['href'].split('/')[-1].split('.')[1]
-        title = ' - '.join([meta['date'], meta['title'], hash])
-        global DIR
-        file_helper.write_article(article, title, DIR)
+        title_id = meta['href'].split('/')[-1].split('.')[1]
+        title = ' - '.join([meta['date'], meta['title'], title_id])
+        dir_path = file_helper.get_dir(sys.argv)
+        file_helper.write_article(article, title, dir_path)
 
 def main():
-    set_data_path()
+    '''Main function.'''
+    setup_path()
     crawler()
 
 if __name__ == '__main__':
