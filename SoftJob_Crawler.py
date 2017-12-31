4f341c3e96cf70948193f2ec642f38a3b46eba14
Kevin Huang <goliath7415@gmail.com>
2018-01-01 01:19:50 +0800

    do refactoring

--- SoftJob_Crawler.py
+++ SoftJob_Crawler.py
@@ -1,24 +1,12 @@
 '''
 Helper functions for file operations.
 '''
-
 import sys
-import time
 
-import requests
-from bs4 import BeautifulSoup
-
-import datetime_helper
 import file_helper
 import ptt
 
-PTT_URL = 'https://www.ptt.cc'
 BOARD_NAME = 'Soft_Job'
-
-AUTHOR = '作者'
-BOARD = '看板'
-TITLE = '標題'
-TIME = '時間'
 
 
 def setup_path():
@@ -29,15 +17,15 @@ def setup_path():
 
 def crawler():
     '''Grab all articles in recent days.'''
-    board = ptt.Board(BOARD_NAME, 5)
+    board = ptt.Board(BOARD_NAME)
 
     while board.url:
         board.get_dom()
         articles_meta = parse_board(board)
 
         for article_meta in articles_meta:
-            article = get_article_content(article_meta)
-            save_article(article, article_meta)
+            article = retrieve_article(**article_meta)
+            save_article(article, **article_meta)
 
 
 def parse_board(board):
@@ -49,65 +37,23 @@ def parse_board(board):
     return board.remove_expired(articles_meta)
 
 
-def get_web_page(url, t=0.4):
-    '''Get web page content.'''
-    # to avoid being detected as DDOS
-    time.sleep(t)
-    resp = requests.get(PTT_URL + url)
+def retrieve_article(**article_meta):
+    if not article_meta:
+        return None
 
-    if resp.status_code == 200:
-        return resp.text
-    print('Invalid URL:', resp.url)
-    return None
+    article = ptt.Article(BOARD_NAME, **article_meta)
+    article.get_dom()
+    return article.format_article()
 
 
-def get_article_content(article_meta):
-    '''Get complete article content.'''
-
-    def get_create_time(dom):
-        metalines = dom.find_all('div', 'article-metaline')
-
-        for metaline in metalines:
-            article_meta_tag = metaline.find('span', 'article-meta-tag')
-
-            if article_meta_tag.text == TIME:
-                return metaline.find('span', 'article-meta-value').text
-
-        return None # TODO gen_date
-
-    def combine(prefix, meta):
-        return '  '.join([prefix, meta])
-
-    def format_article(dom):
-        before, sep, after = dom.text.partition('\n')
-        create_time = get_create_time(dom)
-
-        meta = []
-        meta.append(combine(AUTHOR, article_meta['author']))
-        meta.append(combine(BOARD, BOARD_NAME))
-        meta.append(combine(TITLE, article_meta['title']))
-        meta.append(combine(TIME, create_time) + '\n\n')
-        meta.append(after)
-
-        return sep.join(meta)
-
-    article_page = get_web_page(article_meta['href'])
-
-    if article_page:
-        soup = BeautifulSoup(article_page, 'html.parser')
-        article = soup.find(id='main-content')
-        return format_article(article)
-
-    return None
-
-
-def save_article(article, meta):
+def save_article(article, **meta):
     '''Save cached article to file.'''
     if article:
         # to avoid the titles collision
         # format PTT_URL/..../M.number.A.RND.html, take the number part
         title_id = meta['href'].split('/')[-1].split('.')[1]
         title = ' - '.join([meta['date'], meta['title'], title_id])
+
         dir_path = file_helper.get_dir(sys.argv)
         file_helper.write_article(article, title, dir_path)
 
--- tests/SoftJob_Crawler_test.py
+++ tests/SoftJob_Crawler_test.py
@@ -1,10 +1,58 @@
+import json
+import os
 import unittest
+from file_helper import DEFAULT_DIR
 import SoftJob_Crawler
+import ptt
+
+
+TEST_DIR = 'tests'
+
+
+def read_file(filename):
+    with open(os.path.join(TEST_DIR, filename), 'r', encoding='utf-8') as file:
+        return file.read()
+
+
+def load_json(filename):
+    with open(os.path.join(TEST_DIR, filename), 'r', encoding='utf-8') as file:
+        return json.load(file)
+
 
 class SoftJob_CrawlerTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.path = 'test_path'
+
+        cls.pages = {}
+        cls.pages[0] = read_file('testdata_input_article_1.html')
+        cls.pages[1] = read_file('testdata_input_article_2.html')
+
+        cls.meta = {}
+        cls.meta[0] = load_json('article_meta_1.json')
+        cls.meta[1] = load_json('article_meta_2.json')
+
+        cls.expects = {}
+        cls.expects[0] = load_json('expect_crawler_1.json')
+        cls.expects[1] = load_json('expect_crawler_2.json')
+
+        cls.contents = {}
+        cls.articles = {}
+        for index, article_meta in enumerate(cls.meta.values()):
+            cls.articles[index] = ptt.Article(article_meta['board_name'], **article_meta['article_meta'])
+            cls.articles[index].get_content(cls.pages[index])
+            cls.contents[index] = cls.articles[index].format_article()
+
+
+    def test_setup_path(self):
+        SoftJob_Crawler.setup_path()
+        self.assertEqual(os.path.isdir(DEFAULT_DIR), True)
+
+
     def test_crawler(self):
         #SoftJob_Crawler.crawler()
-        self.fail("Not implemented")
+        #self.fail("Not implemented")
+        pass
 
 
     def test_parse_board(self):
@@ -12,26 +60,28 @@ class SoftJob_CrawlerTest(unittest.TestCase):
         self.assertEqual(articles_meta, None)
 
 
-    def test_get_web_page(self):
-        url = ''
-        self.assertGreater(len(SoftJob_Crawler.get_web_page(url)), 0)
-        url = '/bbs/wtgh/index.html'
-        self.assertEqual(SoftJob_Crawler.get_web_page(url), None)
+    def test_retrieve_article(self):
+        articles = SoftJob_Crawler.retrieve_article()
+        self.assertEqual(articles, None)
+
+        for index, article in enumerate(self.articles.values()):
+            self.retrieve_article(self.meta[index]['article_meta'], self.contents[index])
 
 
-    def test_articles_meta(self):
-        #SoftJob_Crawler.get_articles_meta()
-        self.fail("Not implemented")
-
-
-    def test_get_article_content(self):
-        #SoftJob_Crawler.get_article_content()
-        self.fail("Not implemented")
+    def retrieve_article(self, meta, expect):
+        article_content = SoftJob_Crawler.retrieve_article(**meta)
+        self.assertEqual(article_content, expect)
 
 
     def test_save_article(self):
-        #SoftJob_Crawler.save_article()
-        self.fail("Not implemented")
+        for index, content in enumerate(self.contents.values()):
+            self.save_article(content, self.meta[index]['article_meta'], self.expects[index]['filename'])
+
+
+    def save_article(self, content, meta, expect):
+        SoftJob_Crawler.save_article(content, **meta)
+        self.assertEqual(os.path.isfile(os.path.join(DEFAULT_DIR, expect)), True)
+
 
 if __name__ == '__main__':
     unittest.main()
