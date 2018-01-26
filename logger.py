f3c2b6844a38b52554490e0cb9ec5ecbfd751a3b
Kevin Huang <goliath7415@gmail.com>
2018-01-26 11:25:54 +0800

    add function to stop log for some modules ex. requests

--- logger.py
+++ logger.py
@@ -12,3 +12,11 @@ def load_config():
 def get_logger(module_name):
     '''Get logger with certain name.'''
     return logging.getLogger('.'.join(['crawler', module_name]))
+
+
+def stop_log(module_name):
+    '''Prevent log for module.'''
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.CRITICAL)
+    logger.addFilter(logging.NullHandler())
+    logger.propagate = False
