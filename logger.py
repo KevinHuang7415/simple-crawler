20256c4d33c478187f045ea01ce622d03b95d330
Kevin Huang <goliath7415@gmail.com>
2018-03-15 22:44:24 +0800

    Make crawler setting use setting from Django app

--- logger.py
+++ logger.py
@@ -3,85 +3,17 @@ Helper functions for built-in logging module.
 '''
 import logging
 import logging.config
+from pysite.loggers.helpers import LOGGING as LOG_SETTING
+import pysite.loggers.helpers as log_helper
+
+
+get_logger = log_helper.get_logger
+stop_log = log_helper.stop_log
 
 
 def load_config():
     '''Load configuration for logging.'''
-
-    class PackageFilter(logging.Filter):
-        '''Custom logging filter.'''
-
-        def __init__(self, param='crawler'):
-            super().__init__()
-            self.param = param
-
-        def filter(self, record):
-            if self.param is None:
-                return True
-            return record.getMessage().startswith(self.param)
-
-    logging_config = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-            }
-        },
-        'filters': {
-            'packageFilter': {
-                '()': PackageFilter,
-                "param": "crawler."
-            }
-        },
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
-                'level': 'INFO',
-            },
-            'file': {
-                'class': 'logging.handlers.RotatingFileHandler',
-                'formatter': 'default',
-                'level': 'DEBUG',
-                'filename': 'log\\ptt.log',
-                'mode': 'a',
-                'encoding': 'utf-8',
-            },
-        },
-        'loggers': {
-            'crawler.console': {
-                'handlers': ['console'],
-                'propagate': False,
-                'level': 'INFO',
-                'filters': ['packageFilter']
-            },
-            'crawler.file': {
-                'handlers': ['file'],
-                'level': 'DEBUG',
-                'propagate': False,
-                'filters': ['packageFilter']
-            }
-        },
-        'root': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG'
-        }
-    }
-    logging.config.dictConfig(logging_config)
-
-
-def get_logger(module_name):
-    '''Get logger with certain name.'''
-    return logging.getLogger('.'.join(['crawler', module_name]))
-
-
-def stop_log(module_name):
-    '''Prevent log for module.'''
-    logger = logging.getLogger(module_name)
-    logger.setLevel(logging.CRITICAL)
-    logger.addFilter(logging.NullHandler())
-    logger.propagate = False
+    logging.config.dictConfig(LOG_SETTING)
 
 
 def shutdown():
--- settings.py
+++ settings.py
@@ -5,18 +5,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'PttBoardsEntity',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-        'CHARSET': 'UTF8',
-    }
-}
-
 INSTALLED_APPS = ('data',)
 
 SECRET_KEY = 'REPLACE_ME'
+
+from pysite.pysite.local_settings import DATABASES
