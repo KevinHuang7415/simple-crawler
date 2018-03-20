8bb27a7ad18ef4d2dac229b31d675b555d5e15e2
Kevin Huang <goliath7415@gmail.com>
2018-03-20 22:16:44 +0800

    Add search functions

--- pysite/articles/views.py
+++ pysite/articles/views.py
@@ -2,9 +2,11 @@
 View functions for this application.
 '''
 from datetime import datetime
+import string
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 import loggers.helpers as log_helper
 from .models import SoftJob
 
@@ -53,3 +55,40 @@ def article_detail(request, article_id):
             'url_full': url_full,
             'year': datetime.now().year
         })
+
+
+def articles_search(request, key_word='', page=1):
+    '''Page of articles search list.'''
+    if not acceptable_key_word(key_word):
+        LOGGER.info('Key word [%s] is in ill form.', key_word)
+        return render(request, 'Error.html', {
+            'reason': f'Non-acceptable word: "{key_word}"',
+            'year': datetime.now().year
+        })
+
+    articles_list = SoftJob.objects.filter(
+        Q(title__contains=key_word) | Q(content__contains=key_word)
+    )
+
+    paginator = Paginator(articles_list, 20)
+    if page not in paginator.page_range:
+        LOGGER.info('Page [%d] is out of range.', page)
+        return render(request, 'Error.html', {
+            'reason': 'Wrong page.',
+            'year': datetime.now().year
+        })
+
+    articles = paginator.get_page(page)
+    LOGGER.info('Return page [%d] of search result for [%s].', page, key_word)
+    return render(request, 'search.html', {
+        'articles': articles,
+        'key_word': key_word,
+        'year': datetime.now().year
+    })
+
+
+def acceptable_key_word(key_word):
+    '''Check if the key word for searching is acceptable.'''
+    return key_word and\
+        isinstance(key_word, str) and\
+        len([ch for ch in key_word if ch in string.punctuation]) == 0
--- pysite/pysite/urls.py
+++ pysite/pysite/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.urls import path
 # from articles.demo_index import this_is_a_demo
-from articles.views import articles_listing, article_detail
+from articles.views import articles_listing, article_detail, articles_search
 
 urlpatterns = [
     # path('demo/', this_is_a_demo),
     path('', articles_listing),
     path('articles/', articles_listing),
     path('articles/<int:page>', articles_listing),
-    path('articles/detail/<int:article_id>', article_detail)
+    path('articles/detail/<int:article_id>', article_detail),
+    path('articles/search/', articles_search),
+    path('articles/search/<str:key_word>/', articles_search),
+    path('articles/search/<str:key_word>/<int:page>', articles_search),
 ]
